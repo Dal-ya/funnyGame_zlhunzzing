9d18961d4d7957228f6866a970f71d9a48f364f5
zlhunzzing <zlhunzzing@gmail.com>
2019-11-27 22:34:44 +0900

    move movegit add .

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let game = (function () {
       power: 5,
       exp: 0
     }
-    let monsters = [
+    let enemes = [
       {
         name: '쥐',
         level: 1,
@@ -32,7 +32,7 @@ let game = (function () {
         exp: 10
       }
     ]
-    let monster = null
+    let eneme = null
     let turn = true
 
     return {
@@ -67,10 +67,10 @@ let game = (function () {
         newMessage.innerHTML = msg
         message.prepend(newMessage)
 
-        newMessage.className = 'showMessage'
+        newMessage.className = 'fadeIn'
 
         window.setTimeout(function () {
-          newMessage.className = 'hideMessage'
+          newMessage.className = 'fadeOut'
 
           window.setTimeout(function () {
             message.childNodes[message.childNodes.length-1].remove()
@@ -79,21 +79,23 @@ let game = (function () {
 
 
         if(message.childNodes.length > 10) {
-          message.childNodes[10] = 'hideMessage'
+          message.childNodes[10] = 'fadeOut'
           message.childNodes[10].style.display = 'none';
         }
-
         return this
       },
-      viewUser: function() {
+      showUser: function() {
+        let userFaceImg = document.createElement('img')
+        userFaceImg.src = 'https://user-images.githubusercontent.com/55573219/69720188-64d82280-1155-11ea-893b-36a85ba0625f.png'
+        userFace.appendChild(userFaceImg)
         let userImg = document.createElement('img')
-        userImg.src = 'https://user-images.githubusercontent.com/55573219/69599118-396b1000-104f-11ea-8757-1c5779b71c00.png'
-        viewUser.appendChild(userImg)
+        userImg.src = 'https://user-images.githubusercontent.com/55573219/69720180-6275c880-1155-11ea-80aa-8e4a0e9a88b8.gif'
+        showUser.appendChild(userImg)
       },
       toggleMenu: function() {
         document.querySelector('.startScreen').style.display = 'none'
-        document.querySelector('.screen').style.display = 'block'
-        if(monster) {
+        document.querySelector('#gameScreen').style.display = 'block'
+        if(eneme) {
           if(document.querySelector('.battleBar').style.display === 'block') {
             document.querySelector('.battleBar').style.display = 'none'
             document.querySelector('.nextTurnBar').style.display = 'block'
@@ -112,18 +114,64 @@ let game = (function () {
         }
         return this
       },
-      generateMonster: function() {
-        monster = JSON.parse(JSON.stringify(monsters[Math.floor(Math.random() * monsters.length)]))
-        document.querySelector('.monsterName').innerHTML = '몬스터 : ' + monster.name
-        document.querySelector('.monsterHp').innerHTML = '/ 체력 : ' + monster.hp
-        document.querySelector('.monsterPower').innerHTML = '/ 공격력 : ' + monster.power
-        this.showMessage(monster.name + ' 이(가) 위협해왔다.')
+      generateEneme: function() {
+        eneme = JSON.parse(JSON.stringify(enemes[Math.floor(Math.random() * enemes.length)]))
+        let enemeStat = document.createElement('div')
+        enemeStat.id = 'enemeStatus'
+        enemeInfo.appendChild(enemeStat)
+        enemeStatus.className = 'fadeIn'
+
+        let enemeName = document.createElement('span')
+        enemeName.className = 'enemeName'
+        enemeName.innerHTML = '* 몬스터 : ' + eneme.name
+        enemeStatus.appendChild(enemeName)
+        let enemeHp = document.createElement('span')
+        enemeHp.className = 'enemeHp'
+        enemeHp.innerHTML = ' / 체력 : ' + eneme.hp
+        enemeStatus.appendChild(enemeHp)
+        let enemePower = document.createElement('span')
+        enemePower.className = 'enemePower'
+        enemePower.innerHTML = ' / 공격력 : ' + eneme.power
+        enemeStatus.appendChild(enemePower)
+     
+        this.showEneme().showMessage(eneme.name + ' 이(가) 위협해왔다.')
         return this
       },
-      attackMonster: function() {
-        monster.hp -= user.power
-        document.querySelector('.monsterHp').innerHTML = 'HP:' + monster.hp
-        if(monster.hp > 0) {
+      showEneme: function() {
+        let enemeShow = document.createElement('div')
+        enemeShow.id = 'showEneme'
+
+        let enemeImg = document.createElement('img')
+
+        if(eneme.name === '쥐') {
+          enemeImg.src = 'https://user-images.githubusercontent.com/55573219/69724376-2b0c1980-115f-11ea-8fdb-5c314ccef8c6.gif'
+        }
+        if(eneme.name === '좀비') {
+          enemeImg.src = 'https://user-images.githubusercontent.com/55573219/69724449-46772480-115f-11ea-8c87-548fa7a1d6e9.gif'
+        }
+        if(eneme.name === '[Boss]늑대인간') {
+          enemeImg.src = 'https://user-images.githubusercontent.com/55573219/69724479-55f66d80-115f-11ea-8dd6-a11ce22fa55f.gif'
+        }
+        gameScreen.appendChild(enemeShow)
+        showEneme.appendChild(enemeImg)
+        return this
+      },
+      attackEneme: function() {
+        eneme.hp -= user.power
+        showUser.childNodes[0].remove()
+        let userAttack = document.createElement('img')
+        userAttack.src = 'https://user-images.githubusercontent.com/55573219/69721453-36a81200-1158-11ea-95af-4fae9c773833.gif'
+        showUser.appendChild(userAttack)
+        let now = this
+        window.setTimeout(function () {
+          showUser.childNodes[0].remove()
+          let userImg = document.createElement('img')
+          userImg.src = 'https://user-images.githubusercontent.com/55573219/69720180-6275c880-1155-11ea-80aa-8e4a0e9a88b8.gif'
+          showUser.appendChild(userImg)
+        }, 500)
+
+        document.querySelector('.enemeHp').innerHTML = ' / 체력 : ' + eneme.hp
+        if(eneme.hp > 0) {
           this.nextTurn().showMessage(user.power + '의 데미지를 입혔다.')
         } else
         this.win()
@@ -135,7 +183,7 @@ let game = (function () {
           this.toggleMenu()
       
           window.setTimeout(function () {
-            now.showMessage(monster.name + '의 턴입니다.')
+            now.showMessage(eneme.name + '의 턴입니다.')
               
             window.setTimeout(function () {
               now.attackUser()
@@ -154,31 +202,63 @@ let game = (function () {
         return this
       },
       attackUser: function() {
-        user.hp -= monster.power
-        this.showStat().showMessage(monster.power + '의 데미지를 입었다.')
+        user.hp -= eneme.power
+        showEneme.childNodes[0].remove()
+        let enemeAttack = document.createElement('img')
+        if(eneme.name === '쥐') {
+          enemeAttack.src = 'https://user-images.githubusercontent.com/55573219/69724384-2d6e7380-115f-11ea-823c-b13881258722.png'
+        }
+        if(eneme.name === '좀비') {
+          enemeAttack.src = 'https://user-images.githubusercontent.com/55573219/69724456-49721500-115f-11ea-9351-c9373919bbfc.png'
+        }
+        if(eneme.name === '[Boss]늑대인간') {
+          enemeAttack.src = 'https://user-images.githubusercontent.com/55573219/69724493-5d1d7b80-115f-11ea-840c-676dc19499eb.png'
+        }
+        showEneme.appendChild(enemeAttack)
+        let now = this
+        window.setTimeout(function () {
+          showEneme.remove()
+          now.showEneme()
+        }, 500)
+
+        this.showStat().showMessage(eneme.power + '의 데미지를 입었다.')
       },
       win: function() {
-        user.exp += monster.exp
-        this.showMessage(monster.name + '를 사냥해서 경험치' + monster.exp + ' 을(를) 얻었다.').clearMonster().showExp()
+        user.exp += eneme.exp
+        this.showMessage(eneme.name + '를 사냥해서 경험치' + eneme.exp + ' 을(를) 얻었다.').clearEneme().showExp()
       },
-      clearMonster: function () {
-        monster = null;
-        document.querySelector('.monsterName').innerHTML = ''
-        document.querySelector('.monsterHp').innerHTML = ''
-        document.querySelector('.monsterPower').innerHTML = ''
-        this.toggleMenu()
+      clearEneme: function () {
+        eneme = null;
+        enemeStatus.remove()
+        showEneme.className = 'fadeOut'
+        document.querySelector('.battleBar').style.display = 'none'
+        document.querySelector('.nextTurnBar').style.display = 'block'
+        let now = this
+        window.setTimeout(function () {
+          showEneme.remove()
+          document.querySelector('.nextTurnBar').style.display = 'none'
+          document.querySelector('.menuBar').style.display = 'block'
+        }, 2000)
         return this
       },
       gameOver: function() {
-        document.querySelector('.screen').innerHTML = user.name + '은(는) 레벨' + user.level + '에서 죽었습니다. 새로 시작하려면 새로고침하세요'
+        document.querySelector('#gameScreen').innerHTML = user.name + '은(는) 레벨' + user.level + '에서 죽었습니다. 새로 시작하려면 새로고침하세요'
       },
       rest: function() {
-        user.hp = user.maxHp;
-        this.showExp().showMessage('체력이 회복되었다')
+        if(eneme) {
+          user.hp += user.maxHp/5
+          if(user.hp > user.maxHp) {
+            user.hp = user.maxHp
+          }
+          this.showExp().showMessage('체력이 회복되었다')
+        } else {
+          user.hp = user.maxHp;
+          this.showExp().showMessage('체력이 회복되었다')
+        }
         return this
       },
       exit: function() {
-        document.querySelector('.screen').innerHTML = '게임이 종료되었습니다. 새로 시작하시려면 새로고침하세요.'
+        document.querySelector('#gameScreen').innerHTML = '게임이 종료되었습니다. 새로 시작하시려면 새로고침하세요.'
       },
     }
   }
@@ -197,7 +277,7 @@ nameForm.onsubmit = function (e) {
 
   let name = document.querySelector('.nameBox').value
   if (name.trim() && confirm(name.trim() + '(으)로 하시겠습니까?')) {
-    game.getInstance(name).showExp().toggleMenu().viewUser()
+    game.getInstance(name).showExp().toggleMenu().showUser()
   } else {
     alert('캐릭터의 이름을 입력해주세요.')
   }
@@ -206,7 +286,7 @@ nameForm.onsubmit = function (e) {
 document.querySelectorAll('.menu').forEach(element => {
   element.onclick = function() {
     if(element.innerHTML === '모험한다') {
-      game.getInstance().toggleMenu().generateMonster()
+      game.getInstance().toggleMenu().generateEneme()
     }
     if(element.innerHTML === '휴식한다') {
       game.getInstance().rest()
@@ -220,13 +300,13 @@ document.querySelectorAll('.menu').forEach(element => {
 document.querySelectorAll('.battleMenu').forEach(element => {
   element.onclick = function() {
     if(element.innerHTML === '공격한다') {
-      game.getInstance().attackMonster()
+      game.getInstance().attackEneme()
     }
     if(element.innerHTML === '회복한다') {
       game.getInstance().rest().showExp().nextTurn()
     }
     if(element.innerHTML === '도망친다') {
-      game.getInstance().clearMonster().showMessage('도망쳤습니다.')
+      game.getInstance().clearEneme().showMessage('도망쳤습니다.')
     }
   }
 })
